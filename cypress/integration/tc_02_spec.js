c771cd1f104850a24b3ed6cc49c3f4cd44c81dcd
Marcus Cruz <marcusfunctional01@gmail.com>
2020-10-30 23:09:42 +0000

    deleted comments

--- cypress/integration/tc_02_spec.js
+++ cypress/integration/tc_02_spec.js
@@ -15,7 +15,6 @@ describe('Login', function(){
     it('Create a post', function(){
         cy.contains('New Post').click()
         cy.hash().should('include', '#/editor')
-        //cy.location('hash').should('include', '#/editor')
         cy.get('input[placeholder="Article Title"]').type('Test')
         cy.get('input[placeholder="What\'s this article about?"]').type('Test 1')
         cy.get('textarea[placeholder="Write your article (in markdown)"]').type('Test 2')
@@ -26,15 +25,13 @@ describe('Login', function(){
     it('Mark-unmak as favorite', function(){
         cy.get(':nth-child(4) > .nav-link').contains('marcusdemo').click()
         cy.contains('My Articles').should('be.visible')
-        //cy.get(':nth-child(4) > .nav-link').click
         cy.get(':nth-child(4) > .nav-link').click
         cy.contains('Favorited Articles').click()
         cy.url().should('include', 'favorites')
         cy.get(':nth-child(4) > .nav-link').click
         cy.reload()
 
-        // cy.contains('No articles are here... yet.').should('be.visible')
-        // cy.go('back')
+        
 
 
 

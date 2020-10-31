d039b7e279475b0a66a5e9f5fdb6b565ecc7e701
Marcus Cruz <marcusfunctional01@gmail.com>
2020-10-31 17:49:11 +0000

    deleted a scenario

--- cypress/integration/tc_01_spec.js
+++ cypress/integration/tc_01_spec.js
@@ -9,29 +9,16 @@ describe('Login', function(){
         cy.get(':nth-child(1) > .form-control').type('marcusfunctional01@gmail.com')
         cy.get(':nth-child(2) > .form-control').type('Eusoudorio10*')
         cy.get('.btn').contains('Sign in').should('be.visible').click()
-        cy.contains('Your Feed', {timeout: 10000}).should('be.visible')
+        cy.get('.feed-toggle > .nav > :nth-child(1) > .nav-link')
     })
 
     it('Create a post', function(){
         cy.contains('New Post').click()
         cy.hash().should('include', '#/editor')
-        //cy.location('hash').should('include', '#/editor')
         cy.get('input[placeholder="Article Title"]').type('Test')
         cy.get('input[placeholder="What\'s this article about?"]').type('Test 1')
         cy.get('textarea[placeholder="Write your article (in markdown)"]').type('Test 2')
         cy.contains('Publish Article').click()
-        cy.url().should('include', 'article')
-    })
-
-    it('Mark-unmak as favorite', function(){
-        cy.get(':nth-child(4) > .nav-link').contains('marcusdemo').click()
-        cy.contains('My Articles')//.should('be.visible')
-        cy.get(':nth-child(4) > .nav-link').click
-        cy.contains('Favorited Articles').click()
-        cy.url().should('include', 'favorites')
-        cy.get(':nth-child(4) > .nav-link').click
-        cy.reload()
-
     })
 
     
